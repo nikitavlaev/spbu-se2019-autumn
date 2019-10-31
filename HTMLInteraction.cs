a593daac5ff253be0946ea0445fa1825760aa700
DESKTOP-43GL2DT\NIKITA <nikitavlaev00@gmail.com>
2019-10-31 15:34:02 +0300

    Async HTML interaction

---
+++ HTMLInteraction.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Task04
+{
+    public class HTMLInteraction
+    {
+        public static String GetHTMLbyURL(String urlAddress)
+        {
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                string data = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+
+                return data;
+            }
+            return "";
+        }
+
+        public static List<String> getURLs(String initialURL)
+        {
+            String page = "";
+            try
+            {
+                page = HTMLInteraction.GetHTMLbyURL(initialURL);
+            }
+            catch (WebException)
+            {
+                Console.WriteLine("URL {0} is not valid", initialURL);
+                Environment.Exit(1);
+            }
+
+            Console.WriteLine("Start");
+
+            var anchorRegex = new Regex(
+                @"<a ([^>])*href=""(http|https)://(\S*)""([^>])*>");
+            var urlRegex = new Regex(
+                @"(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?");
+
+            List<String> urls = new List<string>();
+            foreach (Match match in anchorRegex.Matches(page))
+            {
+                String anchor = match.Groups[0].Value;
+                Match linkMatch = urlRegex.Match(anchor);
+                String linkedURL = linkMatch.Groups[0].Value;
+                if (linkedURL == "")
+                {
+                    continue;
+                }
+
+                urls.Add(linkedURL);
+            }
+
+            return urls;
+        }
+    }
+} 
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    class Program
+    {
+        public static String GetData(String linkedURL)
+        {
+            return HTMLInteraction.GetHTMLbyURL(linkedURL);
+        }
+
+        private static Task ProcessURL(String url)
+        {
+            return Task.Run(() => { Console.WriteLine("URL: {0}, Symbols count: {1}", url, GetData(url).Length); }
+            );
+        }
+
+        public static async Task Main(string[] args)
+        {
+            String initialURL = args[0];
+            List<String> urls = HTMLInteraction.getURLs(initialURL);
+            try
+            {
+                List<Task> tasks = new List<Task>();
+                foreach (String url in urls)
+                {
+                    tasks.Add(ProcessURL(url));
+                }
+
+                await Task.WhenAll(tasks.ToArray());
+            }
+            catch (WebException)
+            {
+                Console.WriteLine("URL is not valid or connection problem happened");
+                return;
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("URL unsupported");
+                return;
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("No response");
+            }
+        }
+    }
+}
