549b2f528a18f4ec9c4bba00b6a7e5b107468169
DESKTOP-43GL2DT\NIKITA <nikitavlaev00@gmail.com>
2019-12-15 17:51:39 +0300

    Folders fixed

--- Task03/Setup.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Task03
-{
-    public class Setup
-    {
-        public const int producersNum = 20;
-        public const int consumersNum = 10;
-        public const int sleepInSec = 1;
-    }
-}
